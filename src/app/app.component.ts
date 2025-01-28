a0b0765234cb7e420a6537aa86850b2c0e75fd3c
Henrick Gourrege - STUDENT <s00259704@atu.ie>
2025-01-28 16:21:38 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'hgtest';
+  title = 'Week 2 - To Do List, Henrick Gourrege, S00259704';
+  
+  
+  thingsToDo = [
+    'Learn JavaScript',
+    'Learn Angular',
+    'Learn Redux'
+
+  ];
+  thingsCompleted = [];
 }
